8d0fa9b23a9709821abb62632f6c242158abc701
Bethania Sanchez <bethasanchez3@gmail.com>
2022-02-03 15:31:42 -0400

    changes

--- src/js/component/achievements.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-import propTypes from "prop-types";
-export const Achievements = ({title,data})=> (
-
-<div className="col text-justify"> {data} <br /> <p >{title}</p> </div>
-
-
-	
-);
