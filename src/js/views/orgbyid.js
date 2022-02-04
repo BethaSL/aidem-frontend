f1978642a50607919a6742dc4e2158b193edf1c0
Bethania Sanchez <bethasanchez3@gmail.com>
2022-02-04 12:27:19 -0400

    Orgbyid view updated

--- src/js/views/orgbyid.js
+++ src/js/views/orgbyid.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState, useEffect } from "react";
 import { Context } from "../store/appContext";
 import { Link, useParams } from "react-router-dom";
+import Foto4 from "../../img/foto4.jpeg";
+
 
 export const Orgbyid = () => {
 
@@ -15,57 +17,50 @@ export const Orgbyid = () => {
     }, [store.organizations, store.orgbyid]);
 
     return (
-        store.orgbyid != undefined && (<div className="container footer-down first-row">
+        store.orgbyid != undefined && (<div className="container first-row">
             <h3 className="text-center"> {store.orgbyid.organization_name} </h3>
-            <div className="row first-single">
-                <div className="col-4">
-                    <img src="https://via.placeholder.com/300x200" className="myprofile-pic" alt="..." />
-                </div>
 
+            <div className="row featurette">
                 <div className="col">
-                    <p>{store.orgbyid.description}</p>
+                    <h5>Description</h5>
+                    <p className="lead">{store.orgbyid.description}</p>
+                    <p>Organization type:  {store.orgbyid.organization_type}</p>
                 </div>
-
-                <hr className="featurette-divider" />
-
                 <div className="col">
-                    <div className="d-flex justify-content-between">
-                        <div>Email:</div>
-                        <div>{store.orgbyid.user.email}</div>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                        <div>Address:</div>
-                        <div>{store.orgbyid.address}</div>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                        <div>Bank Name:</div>
-                        <div>{store.orgbyid.bank_name}</div>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                        <div>Organization type:</div>
-                        <div>{store.orgbyid.organization_type}</div>
-                    </div>
-                    <div className="d-flex justify-content-between">
-                        <div>Phone:</div>
-                        <div>{store.orgbyid.phone}</div>
-                    </div>
-                     <div className="d-flex justify-content-between last-row">
-                        <div>Rif:</div>
-                        <div>{store.orgbyid.rif}</div>
-                    </div>
-
-                    <div className="d-flex justify-content-center">
-                        <Link to="/aid">
-                            <button type="button" className="btn form-button"> Adi'em! </button>
-                        </Link>
-                        <Link to="/organizations">
-                            <button type="button" className="btn form-button"> Go back </button>
-                        </Link>
-                    </div>
-                </div>
-                <div>
+                    <img src={Foto4} className="d-flex single-img"/>
                 </div>
             </div>
-        </div>)
+
+            <hr className="featurette-divider" />
+
+            <div className="row first-single">
+                <div className="col">
+                    <h5> Contact:</h5>
+                    <ul>
+                        <li>Email:  {store.orgbyid.user.email}</li>
+                        <li>Phone:  {store.orgbyid.phone}</li>
+                    </ul>
+                </div>
+
+                <div className="col">
+                    <h5>Bank data:</h5>
+                    <ul>
+                        <li>Bank Name:  {store.orgbyid.bank_name}</li>
+                        <li>Bank Account:  {store.orgbyid.bank_account}</li>
+                        <li>Rif: {store.orgbyid.rif}</li>
+                    </ul>
+                </div>
+
+                <div className="d-flex justify-content-center first-single">
+                    <Link to="/aid">
+                        <button type="button" className="btn form-button"> Adi'em! </button>
+                    </Link>
+                    <Link to="/organizations">
+                        <button type="button" className="btn form-button"> Go back </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+        )
     )
 }
